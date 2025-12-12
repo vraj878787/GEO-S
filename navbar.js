79e9e7d7b0e6d2cfa9ce882fc99f01e9bf08a457
Yash <vrajsudani25@gmail.com>
2025-12-12 16:33:15 +0530

    Update logo text from 'The Razor's Edge' to 'The GEO's Edge'

--- navbar.js
+++ navbar.js
@@ -109,7 +109,7 @@ class CustomNavbar extends HTMLElement {
             </style>
 
             <div class="container">
-                <a href="/" class="logo">The Razor's Edge</a>
+                <a href="/" class="logo">The GEO's Edge</a>
 
                 <div class="nav-links">
                     <a href="#home" class="nav-link">Home</a>
